d470421b65e3e22bf118c61b093150b7e3326225
Albert Krauser <roberts.turner123dn@gmail.com>
2023-12-01 09:14:35 +0700

    table version 2

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Table;
+use App\Models\TableOrder;
 use Illuminate\Http\Request;
 
 class TableController extends Controller
@@ -26,12 +27,22 @@ class TableController extends Controller
         $tables = new Table();
         // $tables->ID = $request->ID;
         $tables->Customer = $request->Customer;
-        $tables->Types = $request->Types;
+        $tables->Types = 1;
         // $tables->Status = $request->Status;
         $tables->Note = $request->Note;
         $tables->Date = $request->Date;
         $tables->Time = $request->Time;
         $tables->save();
+
+        // $tbOrder = new TableOrder();
+        // $tableStatus = $request->Status;
+        // if($tableStatus == 0){
+        //     return redirect('table')->with('success', 'Đặt bàn thành công!');
+        // }elseif($tableStatus == 1){
+        //     return redirect('table')->with('success', 'bàn này đã có người đặt');
+        // }else{
+        //     return redirect('home');
+        // }
         return redirect('table')->with('success', 'Thêm mới thành công!');
     }
 }
--- resources/views/navbar/table.blade.php
+++ resources/views/navbar/table.blade.php
@@ -17,84 +17,97 @@
 
 <section class="section">
   <div class="container">
-      <br>
-      <br>
-      <div class="row">
-          <div class="col-md-8">
-              <div class="contact-form">
-                  <form id="contact" action="{{ route('orderTable') }}" method="post">
-                    @csrf
-                    <div class="row">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @elseif (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+    @endif
+    <br>
+    <br>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="contact-form">
+                <form id="contact" action="{{ route('orderTable') }}" method="post">
+                  @csrf
+                  <div class="row">
 
-                      <div class="col-md-6 col-sm-12">
-                        <fieldset>
-                          <input name="Date" type="date" id="date" placeholder="31.07.2020" required="" data-format="YYYY-MM-DD">
-                        </fieldset>
-                      </div>
-
-                      <div class="col-md-6 col-sm-12">
-                        <fieldset>
-                          <input name="Time" type="time" id="time" placeholder="20:30" required="" min="00:00" max="23:59">
-                        </fieldset>
-                      </div>
-
-                      <div class="col-md-6 col-sm-12">
-                        <fieldset>
-                          <select name="Types">
-                              <option value="1">Normal Table</option>
-                              <option value="2">Economy Table</option>
-                              <option value="3">V.I.P Table</option>
-                          </select>
-                        </fieldset>
-                      </div>
-
-                      {{-- <div class="col-md-6 col-sm-12">
-                        <fieldset>
-                          <select name="Status">
-                              <option value="1">Còn trống</option>
-                              <option value="2">Trùng bàn</option>
-                          </select>
-                        </fieldset>
-                      </div> --}}
-
-                      <div class="col-md-6 col-sm-12">
-                        <fieldset>
-                          <input name="Customer" type="text" id="name" placeholder="Name*" required="">
-                        </fieldset>
-                      </div>
-                      
-                      <div class="col-lg-12">
-                        <fieldset>
-                          <textarea name="Note" rows="6" id="notes" placeholder="Notes" required=""></textarea>
-                        </fieldset>
-                      </div>
-                      <div class="col-lg-12">
-                        <fieldset>
-                          <button type="submit" id="form-submit" class="main-button">Check availability</button>
-                        </fieldset>
-                      </div>
+                    <div class="col-md-6 col-sm-12">
+                      <fieldset>
+                        <input name="Date" type="date" id="date" placeholder="31.07.2020" required="" data-format="YYYY-MM-DD">
+                      </fieldset>
                     </div>
-                  </form>
-              </div>
 
-              <br>
-          </div>
+                    <div class="col-md-6 col-sm-12">
+                      <fieldset>
+                        <input name="Time" type="time" id="time" placeholder="20:30" required="" min="00:00" max="23:59">
+                      </fieldset>
+                    </div>
 
-          <div class="col-md-4">
-              <div class="tabs-content">
-                  <label>Email</label>
-                  <p><a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=longthaithien98@gmail.com">longthaithien98@gmail.com</a></p>
+                    <div class="col-md-6 col-sm-12">
+                      <fieldset>
+                        <select name="Types">
+                            <option value="1">Normal Table</option>
+                            <option value="2">Economy Table</option>
+                            <option value="3">V.I.P Table</option>
+                        </select>
+                      </fieldset>
+                    </div>
 
-                  <label>Phone</label>
-                  <p><a href="#"> +1 333 4040 5566</a></p>
+                    {{-- <div class="col-md-6 col-sm-12">
+                      <fieldset>
+                        <select name="Status">
+                            <option value="1">Còn trống</option>
+                            <option value="2">Trùng bàn</option>
+                        </select>
+                      </fieldset>
+                    </div> --}}
 
-                  <label>Address</label>
-                  <p> 212 Barrington Court New York, ABC 10001 United States of America</p>
-              </div>
+                    <div class="col-md-6 col-sm-12">
+                      <fieldset>
+                        <input name="Customer" type="text" id="name" placeholder="Name*" required="">
+                      </fieldset>
+                    </div>
+                    
+                    <div class="col-lg-12">
+                      <fieldset>
+                        <textarea name="Note" rows="6" id="notes" placeholder="Notes" required=""></textarea>
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-12">
+                      <fieldset>
+                        <button type="submit" id="form-submit" class="main-button">Check availability</button>
+                      </fieldset>
+                    </div>
+                  </div>
+                </form>
+            </div>
 
-              <br>
-          </div>
-      </div>
+            <br>
+        </div>
+
+        <div class="col-md-4">
+            <div class="tabs-content">
+                <label>Email</label>
+                <p><a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=longthaithien98@gmail.com">longthaithien98@gmail.com</a></p>
+
+                <label>Phone</label>
+                <p><a href="#"> +1 333 4040 5566</a></p>
+
+                <label>Address</label>
+                <p> 212 Barrington Court New York, ABC 10001 United States of America</p>
+            </div>
+
+            <br>
+        </div>
+    </div>
   </div>
 </section>
 @endsection
